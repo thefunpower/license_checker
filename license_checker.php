02f74103d52ade1471baab233e1f61386757b810
SUNKANG <sunkang@wstaichi.com>
2023-01-07 01:24:59 +0800

    Update license_checker.php

--- license_checker.php
+++ license_checker.php
@@ -1,7 +1,8 @@
 <?php 
 /*
     Copyright (c) 2021-2031, All rights reserved.
-    This is NOT a freeware, use is subject to license terms 
+    This is NOT a freeware
+    LICENSE: https://github.com/thefunpower/core/blob/main/LICENSE.md 
     Connect Email: sunkangchina@163.com 
     Code Vesion: v1.0.x 
 */ 
